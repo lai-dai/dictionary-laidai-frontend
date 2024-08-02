24efea9b4780e0e347b7ab643dd6ab0a618abf01
dailc <dailai.cyno@gmail.com>
2024-08-02 22:56:45 +0700

    #fix api

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,12 @@
 import { createFetcher } from './utils/fetcher'
 
+const nextHost = process.env.NEXT_PUBLIC_BASE_URL
+const apiHost = process.env.NEXT_PUBLIC_API_URL
+
+export const nextApi = createFetcher({
+  baseUrl: nextHost,
+})
+
 export const api = createFetcher({
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  baseUrl: apiHost,
 })
