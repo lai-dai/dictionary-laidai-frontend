d227277a2804b2f0e2cf1d2ad2d0d1e683b38b43
laidai <laidai9966@gmail.com>
2024-08-15 08:14:38 +0700

    update form

--- app/(dashboard)/dashboard/words/create/page.tsx
+++ app/(dashboard)/dashboard/words/create/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { DashboardPageContainer } from '@/components/page-container'
+import { PageForm } from '../_components/form'
 
-export default function Page() {
-  
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { total: string }
+}) {
   return (
-    <>
-      Create
-    </>
+    <DashboardPageContainer>
+      <PageForm isCreateData total={Number(searchParams.total)} />
+    </DashboardPageContainer>
   )
 }
---
+++ components/ui/sortable-list.tsx
@@ -0,0 +1,240 @@
+/**
+ * Part of this code is taken from https://codesandbox.io/u/clauderic
+ */
+import React, {
+  Fragment,
+  forwardRef,
+  useMemo,
+  useState,
+  HTMLAttributes,
+  ReactElement,
+} from 'react'
+import {
+  DndContext,
+  DndContextProps,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragOverlay,
+  DragOverlayProps,
+  defaultDropAnimationSideEffects,
+  DraggableSyntheticListeners,
+  Active,
+  DropAnimation,
+  DragEndEvent,
+  UniqueIdentifier,
+  DragStartEvent,
+  SensorDescriptor,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  SortableContextProps,
+  arrayMove,
+  sortableKeyboardCoordinates,
+  useSortable,
+  UseSortableArguments,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
+import { cn } from '@/lib/utils'
+import { createContext } from '@/lib/utils/create-context'
+import { mergeRefs } from '@/lib/hooks/use-merged-refs'
+
+import { Button, ButtonProps } from '@/components/ui/button'
+
+interface SortableListProps<TData extends Record<string, any>>
+  extends Omit<HTMLAttributes<HTMLUListElement>, 'children'> {
+  sensors?: (SensorDescriptor<any> | undefined | null)[]
+  items?: TData[]
+  idKey?: keyof TData
+  children?: (item: TData, index: number, array: TData[]) => ReactElement
+  onSortChange?: (items: TData[], form: number, to: number) => void
+  dndContextProps?: DndContextProps
+  sortableContextProps?: Omit<SortableContextProps, 'items'>
+  dragOverlayProps?: DragOverlayProps
+}
+
+export function SortableList<TData extends Record<string, any>>({
+  sensors = [],
+  items = [],
+  idKey = 'id',
+  children,
+  onSortChange,
+  dndContextProps,
+  sortableContextProps,
+  dragOverlayProps,
+  className,
+  ...props
+}: SortableListProps<TData>) {
+  if (!(children instanceof Function)) {
+    throw new Error('children must be Function')
+  }
+
+  const [active, setActive] = useState<Active | null>(null)
+
+  const _sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+    ...sensors
+  )
+
+  const activeItem = useMemo(() => {
+    return items.find((item) => item?.[idKey] === active?.id)
+  }, [active, items, idKey])
+
+  const activeIndex = useMemo(() => {
+    return items.findIndex((item) => item?.[idKey] === active?.id)
+  }, [active, items, idKey])
+
+  return (
+    <DndContext
+      sensors={_sensors}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+      {...dndContextProps}
+    >
+      <SortableContext
+        items={items.map((item) => ({ id: item?.[idKey] }))}
+        strategy={verticalListSortingStrategy}
+        {...sortableContextProps}
+      >
+        <ul
+          role="application"
+          className={cn('list-none', className)}
+          {...props}
+        >
+          {items.map((item, index, arr) => (
+            <Fragment key={item?.[idKey] || index}>
+              {children(item, index, arr)}
+            </Fragment>
+          ))}
+        </ul>
+      </SortableContext>
+
+      <SortableOverlay
+        key={activeItem?.[idKey] || activeIndex}
+        {...dragOverlayProps}
+      >
+        {activeItem ? children(activeItem, activeIndex, items) : null}
+      </SortableOverlay>
+    </DndContext>
+  )
+
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event
+    setActive(active)
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+
+    if (over && active.id !== over?.id) {
+      const oldIndex = items.findIndex((item) => item[idKey] === active.id)
+      const newIndex = items.findIndex((item) => item[idKey] === over.id)
+      const newItems = arrayMove(items, oldIndex, newIndex)
+
+      onSortChange?.(newItems, oldIndex, newIndex)
+    }
+    setActive(null)
+  }
+
+  function handleDragCancel() {
+    setActive(null)
+  }
+}
+
+const dropAnimationConfig: DropAnimation = {
+  sideEffects: defaultDropAnimationSideEffects({
+    styles: {
+      active: {
+        opacity: '0.4',
+      },
+    },
+  }),
+}
+
+export function SortableOverlay(props: DragOverlayProps) {
+  return <DragOverlay dropAnimation={dropAnimationConfig} {...props} />
+}
+
+interface SortableItemContextParams {
+  attributes: Record<string, any>
+  listeners: DraggableSyntheticListeners
+  setActivatorNodeRef(node: HTMLElement | null): void
+}
+
+const [SortableItemProvider, useSortableItemContext] =
+  createContext<SortableItemContextParams>({
+    attributes: {},
+    listeners: undefined,
+    setActivatorNodeRef() {},
+  })
+
+interface SortableItemProps extends Omit<HTMLAttributes<HTMLLIElement>, 'id'> {
+  opts?: UseSortableArguments
+  id: UniqueIdentifier
+}
+
+export const SortableItem = forwardRef<HTMLLIElement, SortableItemProps>(
+  function SortableItem({ children, opts, style, id, ...props }, ref) {
+    const {
+      attributes,
+      isDragging,
+      listeners,
+      setNodeRef,
+      setActivatorNodeRef,
+      transform,
+      transition,
+    } = useSortable({
+      id,
+      ...opts,
+    })
+    return (
+      <SortableItemProvider
+        value={{
+          attributes,
+          listeners,
+          setActivatorNodeRef,
+        }}
+      >
+        <li
+          ref={mergeRefs(ref, (r) => setNodeRef(r))}
+          style={{
+            opacity: isDragging ? 0.4 : undefined,
+            transform: CSS.Translate.toString(transform),
+            transition,
+            ...style,
+          }}
+          {...props}
+        >
+          {children}
+        </li>
+      </SortableItemProvider>
+    )
+  }
+)
+
+export const SortableDragTrigger = forwardRef<HTMLButtonElement, ButtonProps>(
+  function SortableDragTrigger({ children, className, ...props }, ref) {
+    const { attributes, listeners, setActivatorNodeRef } =
+      useSortableItemContext()
+
+    return (
+      <Button
+        ref={mergeRefs(ref, (r) => setActivatorNodeRef(r))}
+        className={cn('touch-none cursor-move', className)}
+        suppressHydrationWarning
+        {...attributes}
+        {...listeners}
+        {...props}
+      >
+        {children}
+      </Button>
+    )
+  }
+)
